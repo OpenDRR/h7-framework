067cecb5e70b199bf10b3c1f4e0c535533555e51
Phil <phil-evans@users.noreply.github.com>
2022-09-15 09:26:56 -0300

    11-15 values for spacing setting field

--- resources/functions/fields/builder/settings.php
+++ resources/functions/fields/builder/settings.php
@@ -310,6 +310,11 @@ $GLOBALS['fw_fields']['common']['settings']['spacing'] = array(
 							8 => '8',
 							9 => '9',
 							10 => '10',
+							11 => '11',
+							12 => '12',
+							13 => '13',
+							14 => '14',
+							15 => '15'
 						),
 						'default_value' => false,
 						'allow_null' => 0,
